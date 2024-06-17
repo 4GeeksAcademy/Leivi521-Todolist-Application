952b2b8603aa422d1b81bd7ea12e837ebc6df740
Leivi521 <165715355+Leivi521@users.noreply.github.com>
2024-06-17 22:43:47 +0000

    Done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import reactDom from "react"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+
+  const handleAddTask = () => {
+    // Check if input value is empty before adding to list
+    if (inputValue.trim()) { 
+      setToDoList([...toDoList, inputValue]);
+      setInputValue(""); 
+    } else {
+      alert("Please enter a task!"); // Inform user about empty input
+    }
+  }
+  function handleDelete(index) {
+    setToDoList(toDoList.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div className="container">
+      <div className="nav">
+        <h2>To-DO-List</h2>
+        <div className="user-input">
+          <input
+            id="input"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+            type="text"
+            placeholder="Enter a task..." 
+          />
+          <button onClick={handleAddTask}>Add Task</button>
+        </div>
+      </div>
+      <div className="container">
+        <div className="To-do items">
+          <ul>
+            {toDoList.map((task, index) => (
+            <li key={index}>
+            {task} <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+            ))}
+			
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
+
