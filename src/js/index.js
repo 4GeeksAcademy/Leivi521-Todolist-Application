78f59e40b2b1daad926110b21454e86fdf1fe312
Leivi521 <165715355+Leivi521@users.noreply.github.com>
2024-07-01 23:35:39 +0000

    done

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+
+
 //import your own components
 import Home from "./component/home.jsx";
 
@@ -13,3 +15,4 @@ import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
